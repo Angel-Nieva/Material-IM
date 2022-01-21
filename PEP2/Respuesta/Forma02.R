579251508b758c1c0cecf716c968cfb8db657805
Angel <angel.nieva@usach.cl>
2022-01-21 20:23:11 -0300

    3

--- PEP2/Respuesta/Forma02.R
+++ PEP2/Respuesta/Forma02.R
@@ -23,6 +23,10 @@ datos <- read.csv2(arch, fileEncoding = "UTF-8")
 #####     Desarrollo     #####
 ##############################
 
+# Las hipótesis a formular:
+# H0: Las evaluaciones promedio hechas por los capitanes en las distintas divisiones son iguales.
+# HA: Las evaluaciones promedio hechas por los capitanes en las distintas divisiones es diferente para
+#     al menos un algoritmo.
 
 # Se obtienen las distintas divisiones:
 cavetrooper  <- datos %>% filter(division == "Cavetrooper")
@@ -33,5 +37,40 @@ spacetrooper  <- datos %>% filter(division == "Spacetrooper")
 sandtrooper  <- datos %>% filter(division == "Sandtrooper")
 flametrooper  <- datos %>% filter(division == "Flametrooper")
 recontrooper  <- datos %>% filter(division == "Recontrooper")
+
+# Se obtienen las evaluaciones de los soldados por sus capitanes:
+cavetrooper  <- cavetrooper$eval_capitan
+snowtrooper  <- snowtrooper$eval_capitan
+lavatrooper  <- lavatrooper$eval_capitan
+shoretrooper <- shoretrooper$eval_capitan
+spacetrooper <- spacetrooper$eval_capitan
+sandtrooper  <- sandtrooper$eval_capitan
+flametrooper <- flametrooper$eval_capitan
+recontrooper <- recontrooper$eval_capitan
+
+# Se estudia la normalidad de las tropas:
+n1 <- shapiro.test(cavetrooper)
+n2 <- shapiro.test(snowtrooper)
+n3 <- shapiro.test(lavatrooper)
+n4 <- shapiro.test(shoretrooper)
+n5 <- shapiro.test(spacetrooper)
+n6 <- shapiro.test(sandtrooper)
+n7 <- shapiro.test(flametrooper)
+n8 <- shapiro.test(recontrooper)
+
+# n1 p-value = 0.6662
+# n2 p-value = 0.641
+# n3 p-value = 0.5838
+# n4 p-value = 0.1939
+# n5 p-value = 0.554
+# n6 p-value = 0.2406
+# n7 p-value = 0.8942
+# n8 p-value = 0.6213
+
+# Al observar los p-value de la prueba de normalidad podemos concluir que las
+# tropas de las diferentes divisiones se comportan de manera normal.
+
+
+
 
 #cavetrooper <- cavetrooper %<% filter()
